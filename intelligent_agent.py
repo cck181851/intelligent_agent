8bf7142be942fe94c89c6a33a3e1069ac03db39a
Ahmet Burak Çiçek <cck181851@gmail.com>
2024-10-12 02:28:16 +0300

    Update intelligent_agent.py

--- intelligent_agent.py
+++ intelligent_agent.py
@@ -1,4 +1,4 @@
-import heapq,math
+import heapq,math,sys
 
 class Node:
     def __init__(self,pawns,rook,bishop,knight,cost,parent):
@@ -18,17 +18,32 @@ class Node:
     def __hash__(self):
         return hash((str(self.pawns),self.rook,self.bishop,self.knight))    
 
-N=5 
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self.cost == other.cost
+        return 0        
 
-table=[[".","R",".",".","."],[".","1","x",".","."],[".","2","x",".","."],[".","3","x",".","."],[".","4","5","6","."]]
+arguments = sys.argv 
+file_name,input_file_name,output_file_name,method,heuristic=arguments
+
+N=7
+
+table=[[".",".",".","x","R",".","."],
+        [".",".","x","x",".",".","x"],
+        [".",".",".",".",".",".","."],
+        [".",".","x","1",".","2","x"],
+        ["5",".",".",".",".",".","."],
+        [".",".",".",".",".","3","."],
+        [".",".","4",".",".",".","."]]
+copied=[[i for i in j] for j in table]        
 
 knight_move=[[-1,2],[-2,1],[-2,-1],[-1,-2],[1,-2],[2,-1],[2,1],[1,2]]
 
-bishop_move=[[(-i,i) for i in range(1,N+1)],[(-i,-i) for i in range(1,N+1)],
-            [(i,-i) for i in range(1,N+1)],[(i,i) for i in range(1,N+1)]]
+bishop_move=[[(1*i,1*i) for i in range(1,N+1)],[(-1*i,1*i) for i in range(1,N+1)],
+    [(1*i,-1*i) for i in range(1,N+1)],[(-1*i,-1*i) for i in range(1,N+1)]]
 
-rook_move=[[(0,i) for i in range(1,N+1)],[(-i,0) for i in range(1,N+1)],
-            [(0,-i) for i in range(1,N+1)],[(i,0) for i in range(1,N+1)]]
+rook_move=[[(1*i,0) for i in range(1,N+1)],[(-1*i,0) for i in range(1,N+1)],
+    [(0,1*i) for i in range(1,N+1)],[(0,-1*i) for i in range(1,N+1)]]
 
 obstacles=set()
 
@@ -50,128 +65,147 @@ for i in range(N):
 
 total_expanded_nodes=[0]             
 
-def search(mode):
+def search():
     queue=[(0,0,initial)]    
-    seen={initial}
+    seen=set()
 
     while queue:        
-        _,_,node=heapq.heappop(queue)                           
+        _,_,node=heapq.heappop(queue) 
+        print(node.rook)                          
         if len(node.pawns)==0:
             return node  
         if node.knight:
-            expand_knight(node,seen,queue,mode)
+            expand_knight(node,seen,queue)
         if node.bishop:
-            expand_bishop(node,seen,queue,mode)
+            expand_bishop(node,seen,queue)
         if node.rook:
-            expand_rook(node,seen,queue,mode)                                 
+            expand_rook(node,seen,queue)                                 
 
     return None  
 
-def expand_knight(node,seen,queue,mode):
+def expand_knight(node,seen,queue):
     x,y=node.knight
+    if node in seen:
+        return
+    total_expanded_nodes[0]+=1    
+    seen.add(node)  
     for dx,dy in knight_move:
         new_x,new_y=x+dx,y+dy  
         if not (0<=new_x<N and 0<=new_y<N):
             continue
-        if any([target and ((new_x,new_y)==target or (new_x,new_y) in target) for target in 
-            [obstacles,node.rook,node.bishop]]):
-            continue
+        if any([(new_x,new_y)==(i,j) for i,j in obstacles]) or (new_x,new_y)==node.bishop or (new_x,new_y)==node.rook:
+            continue                     
         new_pawns=tuple(pos for pos in node.pawns if pos!=(new_x,new_y)) 
         new_knight=(new_x,new_y)
-        new_cost=knight_find_cost(mode,node,new_x,new_y)        
+        new_cost=knight_find_cost(node,new_x,new_y)        
         new_node=Node(new_pawns,node.rook,node.bishop,new_knight,node.cost+6,node)
-        if new_node not in seen: 
-            seen.add(node) 
-            total_expanded_nodes[0]+=1
+        if new_node not in seen:         
             heapq.heappush(queue,(new_cost,total_expanded_nodes[0],new_node))   
 
-def expand_rook(node,seen,queue,mode):
+def expand_rook(node,seen,queue):
     x,y=node.rook
-    for direction in rook_move:
-        for dx,dy in direction:
+    if node in seen:
+        return
+    total_expanded_nodes[0]+=1    
+    seen.add(node)
+    for change in rook_move:
+        for dx,dy in change:
             new_x=x+dx
             new_y=y+dy            
             if not (0<=new_x<N and 0<=new_y<N):
                 break
-            if any([target and ((new_x,new_y) in target or (new_x,new_y)==target) for target in 
-            [obstacles,node.rook,node.bishop,node.knight]]):
+            if any([(new_x,new_y)==(i,j) for i,j in obstacles]) or (new_x,new_y)==node.bishop or (new_x,new_y)==node.knight:
                 break
             new_pawns=tuple(pos for pos in node.pawns if pos!=(new_x,new_y))
             new_rook=(new_x,new_y)
-            new_cost=rook_find_cost(mode,node,new_x,new_y)
+            new_cost=rook_find_cost(node,new_x,new_y)
             new_node=Node(new_pawns,new_rook,node.bishop,node.knight,node.cost+8,node)
-            if new_node not in seen:            
-                seen.add(node) 
-                total_expanded_nodes[0]+=1
-                heapq.heappush(queue,(new_cost,total_expanded_nodes[0],new_node))   
+            if new_node not in seen:    
+                heapq.heappush(queue,(new_cost,total_expanded_nodes[0],new_node))  
+                if (new_x,new_y) in node.pawns:
+                    break 
 
-def expand_bishop(node,seen,queue,mode):
+def expand_bishop(node,seen,queue):
     x,y=node.bishop
-    for direction in bishop_move:
-        for dx,dy in direction:
+    if node in seen:
+        return
+    seen.add(node)  
+    total_expanded_nodes[0]+=1  
+    for change in bishop_move:
+        for dx,dy in change:
             new_x=x+dx
             new_y=y+dy 
             if not (0<=new_x<N and 0<=new_y<N):
                 break
-            if any([target and ((new_x,new_y) in target or (new_x,new_y)==target) for target in 
-                    [obstacles,node.rook,node.bishop,node.knight]]):
+            if any([(new_x,new_y)==(i,j) for i,j in obstacles]) or (new_x,new_y)==node.knight or (new_x,new_y)==node.rook:
                 break
             new_pawns=tuple(pos for pos in node.pawns if pos!=(new_x,new_y))
             new_bishop=(new_x,new_y)
-            new_cost=bishop_find_cost(mode,node,new_x,new_y)
+            new_cost=bishop_find_cost(node,new_x,new_y)
             new_node=Node(new_pawns,node.rook,new_bishop,node.knight,node.cost+10,node)
-            if new_node not in seen:
-                seen.add(node) 
-                total_expanded_nodes[0]+=1
+            if new_node not in seen:                     
                 heapq.heappush(queue,(new_cost,total_expanded_nodes[0],new_node))   
+                if (new_x,new_y) in node.pawns:
+                    break    
 
-def rook_find_cost(mode,node,new_x,new_y):
-    new_cost=0
-    if mode==1:
-        new_cost=node.cost+8
-    elif mode==2:
-        random=any(pi==new_x or pj==new_y for pi,pj in node.pawns) 
-        new_cost=node.cost+6+8*(len(node.pawns) if random else 1+(len(node.pawns)))
-    else:   
-        new_cost+=node.cost+8 
-        for i,j in node.pawns:
-            cur=8 if new_x==i or new_y==j else 16
-            new_cost+=cur
-    return new_cost
+def rook_find_cost(node,new_x,new_y):    
+    if method=="UCS":
+        return node.cost+8
+    elif method=="GS" and heuristic=="h1":
+        random=any([new_x==pi or new_y==pj for pi,pj in node.pawns])
+        return 8*((len(node.pawns)+(1 if not random else 0)))      
+    elif method=="GS" and heuristic=="h2":
+        new_cost=0
+        for pi,pj in node.pawns:
+            new_cost+=8 if pi==new_x or pj==new_y else 16
+        return new_cost    
+    elif method=="AS" and heuristic=="h1":
+        random=any([new_x==pi or new_y==pj for pi,pj in node.pawns])
+        return (node.cost+8)+8*((len(node.pawns)+(1 if not random else 0)))              
+    elif method=="AS" and heuristic=="h2":
+        new_cost=node.cost+8 
+        for pi,pj in node.pawns:
+            new_cost+=8 if pi==new_x or pj==new_y else 16   
+        return new_cost           
 
-def bishop_find_cost(mode,node,new_x,new_y):
-    new_cost=0
-    if mode==1:
-        new_cost=node.cost+10  
-    elif mode==2:
-        new_cost=node.cost
-    else:
-        new_cost+=node.cost+10
-        for i,j in node.pawns:
-            cur=10 if i==new_x or j==new_y else 20
-            new_cost+=cur
-    return new_cost 
+def bishop_find_cost(node,new_x,new_y):    
+    if method=="UCS":
+        return node.cost+10  
+    elif method=="GS" and heuristic=="h2":
+        new_cost=0
+        for pi,pj in node.pawns:
+            new_cost+=10 if pi==new_x or pj==new_y else 20
+        return new_cost
+    elif method=="AS" and heuristic=="h2":
+        new_cost=node.cost+10
+        for pi,pj in node.pawns:
+            new_cost+=10 if pi==new_x or pj==new_y else 20
+        return new_cost        
 
-def knight_find_cost(mode,node,new_x,new_y):
-    new_cost=0
-    if mode==1:
-        new_cost=node.cost+6 
-    elif mode==2:
-        new_cost=node.cost
-    else:
-        new_cost+=node.cost+6
-        for i,j in node.pawns:
-            dist=abs(new_x-i)+abs(new_y-j)
-            new_cost+=math.ceil(dist/3)*6
-    return new_cost 
 
-res=search(3)
+def knight_find_cost(node,new_x,new_y):
+    if method=="UCS":
+        return node.cost+6 
+    elif method=="GS" and heuristic=="h2":
+        new_cost=0
+        for pi,pj in node.pawns:
+            cur=abs(pi-new_x)+abs(pj-new_y)
+            new_cost+=math.ceil(cur/3)
+        return new_cost    
+    elif method=="AS" and heuristic=="h2":
+        new_cost=node.cost+6
+        for pi,pj in node.pawns:
+            cur=abs(pi-new_x)+abs(pj-new_y)
+            new_cost+=math.ceil(cur/3)
+        return new_cost         
+
+res=search()
 print(res.cost,total_expanded_nodes[0])
 
 while res.parent:
     table=[["." for _ in range(N)] for _ in range(N)]
     for i,j in res.pawns:
-        table[i][j]="1"
+        table[i][j]=copied[i][j]
     for i,j in obstacles:
         table[i][j]="x"
     if res.knight:
@@ -182,8 +216,7 @@ while res.parent:
         table[res.bishop[0]][res.bishop[1]]="B"
     for t in table:        
         print(t)
-    print("********************")
-        
+    print("********************")        
     res=res.parent    
 
 
@@ -198,6 +231,25 @@ while res.parent:
 
 
 
+
+
+ 
+
+
+
+        
+          
+
+
+
+
+
+ 
+
+
+
+        
+          
 
 
 
